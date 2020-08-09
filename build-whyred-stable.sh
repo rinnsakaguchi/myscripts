64f8b386e9ffac5fde589235c593384280aa6a47
predator112 <mi460334@gmail.com>
2020-08-09 15:08:00 +0000

    Revert "Update build-whyred-stable.sh"
    This reverts commit d40daa393b253ac03d42594aa839a88db26351f1.

--- build-whyred-stable.sh
+++ build-whyred-stable.sh
@@ -140,6 +140,7 @@ fixcilto() {
 
 ## Start the kernel buildflow ##
 setversioning
+tg_channelcast "<b>$CIRCLE_BUILD_NUM CI Build Triggered</b>" \
         "Compiler: <code>${COMPILER_STRING}</code>" \
 	"Device: ${DEVICE}" \
 	"Kernel: <code>${KERNEL}, ${KERNELRELEASE}</code>" \
