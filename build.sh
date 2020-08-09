55cba33caf8a167f43a4044ae38db3c1d85a91f1
predator112 <65210380+predator112@users.noreply.github.com>
2020-08-09 21:57:48 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -135,6 +135,7 @@ fixcilto() {
 
 ## Start the kernel buildflow ##
 setversioning
+fixcilto
 tg_channelcast "<b>CI Build Triggered</b>" \
         "Compiler: <code>${COMPILER_STRING}</code>" \
 	"Device: ${DEVICE}" \
