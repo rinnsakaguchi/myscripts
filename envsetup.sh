4a910ddeb2790d218a1fc62011bb651e4f4ba0c3
predator112 <mi460334@gmail.com>
2020-08-09 15:06:33 +0000

    Revert "Update envsetup.sh"
    This reverts commit de36d8300ea53700a3b07ce770d2a6668588c6cb.

--- envsetup.sh
+++ envsetup.sh
@@ -13,9 +13,24 @@
 KERNELDIR="$(pwd)"
 SCRIPTS=${KERNELDIR}/kernelscripts
 OUTDIR=${KERNELDIR}/out
+COMPILER_TYPES=clang
 
-git clone https://github.com/arter97/arm64-gcc.git --depth=1 "${KERNELDIR}/gcc"
-git clone https://github.com/arter97/arm32-gcc.git --depth=1 "${KERNELDIR}/gcc32"
+# Pick your poison
+if [[ "${COMPILER_TYPES}" =~ "clang" ]]; then
+        git clone https://github.com/RaphielGang/aosp-clang.git "${KERNELDIR}"/clang
+        git clone https://github.com/KudProject/aarch64-linux-android-4.9.git "${KERNELDIR}"/gcc
+        git clone https://github.com/KudProject/arm-linux-androideabi-4.9.git "${KERNELDIR}"/gcc32
+        COMPILER_STRING='AARCH64 : GCC 4.9
+        ARM     : GCC 4.9
+        CLANG   : GOOGLE CLANG'
+	COMPILER_TYPE='clang'
+else
+        # Default to GCC from Arter
+        git clone https://github.com/arter97/arm64-gcc --depth=1 "${KERNELDIR}/gcc"
+        git clone https://github.com/arter97/arm32-gcc --depth=1 "${KERNELDIR}/gcc32"
+        COMPILER_STRING='GCC 9.x'
+	COMPILER_TYPE='GCC9.x'
+fi
 
 export COMPILER_STRING COMPILER_TYPE KERNELDIR SCRIPTS OUTDIR
 
