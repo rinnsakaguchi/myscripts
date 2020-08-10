03572e1e6d1fdacdd977c6a680765696e1016012
predator112 <65210380+predator112@users.noreply.github.com>
2020-08-11 00:04:06 +0700

    Create setup-test.sh

---
+++ setup-test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2019 nysascape
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+# Probably the 3rd bad apple coming
+# Enviroment variables
+
+# Export KERNELDIR as en environment-wide thingy
+# We start in scripts, so like, don't clone things there
+KERNELDIR="$(pwd)"
+SCRIPTS=${KERNELDIR}/kernelscripts
+OUTDIR=${KERNELDIR}/out
+
+git clone https://github.com/chips-project/aarch64-linux-gnu.git --depth=1 "${KERNELDIR}/gcc"
+git clone https://github.com/arter97/arm32-gcc.git --depth=1 "${KERNELDIR}/gcc32"
+        COMPILER_STRING='GCC 11.x'
+
+export COMPILER_STRING COMPILER_TYPE KERNELDIR SCRIPTS OUTDIR
+
+git clone https://github.com/fabianonline/telegram.sh/ telegram
+
+# Export Telegram.sh
+TELEGRAM=${KERNELDIR}/telegram/telegram
+
+export TELEGRAM JOBS
