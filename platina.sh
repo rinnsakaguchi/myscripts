47389f96d16790e0ca5cc25d04ffa9c1138ac01a
predator112 <65210380+predator112@users.noreply.github.com>
2020-08-14 06:12:01 +0700

    Update platina.sh

--- platina.sh
+++ platina.sh
@@ -71,7 +71,7 @@ makekernel() {
     rm -rf ${ANYKERNEL}
     git clone https://github.com/PREDATOR-project/AnyKernel3.git -b Phiton-newcam anykernel3
     kernelstringfix
-    export CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-elf-"
+    export CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-linux-android-"
     export CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-eabi-"
     make O=out ARCH=arm64 ${DEFCONFIG}
     make -j$(nproc --all) O=out ARCH=arm64
