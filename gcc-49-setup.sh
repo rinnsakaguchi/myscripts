c6a0c73ac9e1e1af24feb3227476a74107bc070a
predator112 <65210380+predator112@users.noreply.github.com>
2020-08-15 11:32:37 +0700

    Create gcc-49-setup.sh

---
+++ gcc-49-setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2019 nysascape
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+# Probably the 3rd bad apple coming
+# Enviroment variables
+
+# Export KERNELDIR as en environment-wide thingy
+# We start in scripts, so like, don't clone things there
+KERNELDIR="$(pwd)"
+SCRIPTS=${KERNELDIR}/kernelscripts
+OUTDIR=${KERNELDIR}/out
+
+git clone https://github.com/KudProject/aarch64-linux-android-4.9.git --depth=1 "${KERNELDIR}/gcc"
+git clone https://github.com/KudProject/arm-linux-androideabi-4.9.git --depth=1 "${KERNELDIR}/gcc32"
+        COMPILER_STRING='GCC 4.9'
+
+export COMPILER_STRING COMPILER_TYPE KERNELDIR SCRIPTS OUTDIR
+
+git clone https://github.com/fabianonline/telegram.sh/ telegram
+
+# Export Telegram.sh
+TELEGRAM=${KERNELDIR}/telegram/telegram
+
+export TELEGRAM JOBS
