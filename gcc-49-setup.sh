178ab2941c5bb1a82aa077a107b4280f8d990802
predator112 <65210380+predator112@users.noreply.github.com>
2020-08-16 11:26:07 +0700

    Update gcc-49-setup.sh

--- gcc-49-setup.sh
+++ gcc-49-setup.sh
@@ -14,7 +14,7 @@ KERNELDIR="$(pwd)"
 SCRIPTS=${KERNELDIR}/kernelscripts
 OUTDIR=${KERNELDIR}/out
 
-git clone https://github.com/shivamjadon/UBER-TC-4.9.git --depth=1 "${KERNELDIR}/gcc"
+git clone https://github.com/KudProject/aarch64-linux-android-4.9.git --depth=1 "${KERNELDIR}/gcc"
 git clone https://github.com/KudProject/arm-linux-androideabi-4.9.git --depth=1 "${KERNELDIR}/gcc32"
         COMPILER_STRING='GCC 4.9'
 
