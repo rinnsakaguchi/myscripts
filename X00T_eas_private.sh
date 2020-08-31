4d1c19cfe747c0fa697079e421cf5de5bb643235
predator112 <65210380+predator112@users.noreply.github.com>
2020-08-31 13:53:29 +0700

    Update X00T_eas_private.sh

--- X00T_eas_private.sh
+++ X00T_eas_private.sh
@@ -71,8 +71,8 @@ makekernel() {
     rm -rf ${ANYKERNEL}
     git clone https://github.com/PREDATOR-project/AnyKernel3.git -b BULDOSER-X00T-EAS anykernel3
     kernelstringfix
-    CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-linux-android-"
-    export CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-linux-androideabi-"
+    CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-linux-gnu-"
+    export CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-eabi"
     make O=out ARCH=arm64 ${DEFCONFIG}
     make -j$(nproc --all) O=out ARCH=arm64
 
