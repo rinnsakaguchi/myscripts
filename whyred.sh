3b6af9f63939d5e7080eca925330c55df240acbf
predator112 <65210380+predator112@users.noreply.github.com>
2020-10-08 20:48:10 +0700

    telo

--- whyred.sh
+++ whyred.sh
@@ -25,7 +25,7 @@ export KBUILD_BUILD_HOST=Predator_Nusantara🇮🇩
 export OUTFILE=${OUTDIR}/arch/arm64/boot/Image.gz-dtb
 
 # Kernel groups
-CI_CHANNEL=-1001488385343
+CI_CHANNEL=-1001418824983
 
 # Set default local datetime
 DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
