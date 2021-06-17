425041a2e14c542dde4038db33cf218d251e2f38
predator112 <65210380+predator112@users.noreply.github.com>
2021-06-17 12:06:15 +0700

    p

--- ten-kernel.sh
+++ ten-kernel.sh
@@ -79,7 +79,7 @@ makekernel() {
     rm -rf ${ANYKERNEL}
     git clone https://github.com/PREDATOR-project/AnyKernel3.git -b BangBroz-oldcam anykernel3
     kernelstringfix
-    make -j"$(nproc --all)" O=out ${DEFCONFIG} \
+    make -j"$(nproc --all)" O=out ARCH=arm64 ${DEFCONFIG} \
           CC=clang \
           AR=llvm-ar \
           NM=llvm-nm \
