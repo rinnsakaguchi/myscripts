585f8aef8c4d3f144b76265528743f4e072216e4
predator112 <65210380+predator112@users.noreply.github.com>
2021-07-13 12:12:06 +0700

    Update new-base.sh

--- new-base.sh
+++ new-base.sh
@@ -20,8 +20,8 @@ PARSE_ORIGIN="$(git config --get remote.origin.url)"
 COMMIT_POINT="$(git log --pretty=format:'%h : %s' -1)"
 
 # Export custom KBUILD
-export KBUILD_BUILD_USER=CircleCI
-export KBUILD_BUILD_HOST=whyred
+export KBUILD_BUILD_USER=Termux
+export KBUILD_BUILD_HOST=Local_Server
 export OUTFILE=${OUTDIR}/arch/arm64/boot/Image.gz-dtb
 
 # Kernel groups
