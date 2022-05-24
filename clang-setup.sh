12394a0a70b2ad614dec16ace5d157511fdd684f
predator112 <65210380+predator112@users.noreply.github.com>
2022-05-24 14:56:40 +0700

    Update clang-setup.sh

--- clang-setup.sh
+++ clang-setup.sh
@@ -23,8 +23,8 @@ else
         # Default to GCC from Arter
         git clone https://github.com/mvaisakh/gcc-arm64 -b gcc-master --depth=1 "${KERNELDIR}/gcc"
         git clone https://github.com/kdrag0n/arm-eabi-gcc --depth=1 "${KERNELDIR}/gcc32"
-        COMPILER_STRING='GCC 12.x'
-	COMPILER_TYPE='GCC12.x'
+        COMPILER_STRING='GCC 13.x'
+	COMPILER_TYPE='GCC13.x'
 fi
 
 export COMPILER_STRING COMPILER_TYPE KERNELDIR SCRIPTS OUTDIR
