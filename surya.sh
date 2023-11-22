7bb17532cf5450046599c10aa0d9d3adaebbcf2d
predator112 <65210380+predator112@users.noreply.github.com>
2023-11-22 10:35:48 +0700

    Update surya.sh

--- surya.sh
+++ surya.sh
@@ -76,12 +76,7 @@ makekernel() {
     kernelstringfix
     make O=out ARCH=arm64 ${DEFCONFIG}
     if [[ "${COMPILER_TYPE}" =~ "clang"* ]]; then
-        make -j$(nproc --all) O=out \
-                              ARCH=arm64 \
-                              CC=clang \
-                              CLANG_TRIPLE=aarch64-linux-gnu- \
-                              CROSS_COMPILE=aarch64-elf-ar \
-                              CROSS_COMPILE_ARM32=arm-eabi-
+        make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-elf-ar CROSS_COMPILE_ARM32=arm-eabi-                          
     else
 	    make -j$(nproc --all) O=out ARCH=arm64 CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-eabi-"
     fi
