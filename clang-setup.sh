63900ca795b5181ed9ea3e4d21f81acadb3ccdfb
predator112 <65210380+predator112@users.noreply.github.com>
2023-11-23 16:52:16 +0700

    Update clang-setup.sh

--- clang-setup.sh
+++ clang-setup.sh
@@ -19,9 +19,9 @@ COMPILER_TYPES=clang
 if [[ "${COMPILER_TYPES}" =~ "clang" ]]; then
         git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b main --depth=1  "${KERNELDIR}/clang"
 	git clone https://github.com/mvaisakh/gcc-arm64 -b gcc-master --depth=1 "${KERNELDIR}/gcc"
-        git clone https://github.com/kdrag0n/arm-eabi-gcc --depth=1 "${KERNELDIR}/gcc32"
-	COMPILER_STRING='google clang'
-        COMPILER_TYPE='google clang'
+        git clone https://github.com/mvaisakh/gcc-arm -b gcc-master --depth=1 "${KERNELDIR}/gcc32"
+	COMPILER_STRING='google clang 17.0.4'
+        COMPILER_TYPE='google clang 17.0.4'
 else
         # Default to GCC from Arter
         git clone https://github.com/kdrag0n/aarch64-elf-gcc --depth=1 "${KERNELDIR}/gcc"
