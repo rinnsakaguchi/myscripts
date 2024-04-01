89e6ae04ebcdc9b9733809cbe7ec92e0bf29795d
predator112 <65210380+predator112@users.noreply.github.com>
2024-04-01 14:28:20 +0700

    Update surya.sh

--- surya.sh
+++ surya.sh
@@ -108,7 +108,7 @@ makekernel() {
 	OBJCOPY=llvm-objcopy \
 	OBJDUMP=llvm-objdump \
 	CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-none-linux-gnu-" \
-	CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-none-linux-gnueabihf-"
+	CROSS_COMPILE_COMPAT="${KERNELDIR}/gcc32/bin/arm-none-linux-gnueabihf-"
     else
 	    make -j$(nproc --all) O=out ARCH=arm64 CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-eabi-"
     fi
