a2b9e247340cc4ed519f4ad390cb280f2d2c525f
rinnsakaguchi <ffrizz042@gmail.com>
2025-05-28 18:00:53 +0700

    Update clang-setup.sh

--- clang-setup.sh
+++ clang-setup.sh
@@ -19,7 +19,7 @@ COMPILER_TYPES=clang
 if [[ "${COMPILER_TYPES}" =~ "clang" ]]; then
         git clone --depth=1 -b clang-21.0 https://gitlab.com/kutemeikito/rastamod69-clang  "${KERNELDIR}/clang"
 	git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "${KERNELDIR}/gcc"
-    git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git "${KERNELDIR}/gcc32"
+        git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git "${KERNELDIR}/gcc32"
 	COMPILER_STRING='Rasta clang 21'
         COMPILER_TYPE='Rasta clang 21'
 fi    
