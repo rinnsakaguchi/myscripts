9e08aca0f98e540d519f85e1cf911427658ad4a8
rinnsakaguchi <ffrizz042@gmail.com>
2025-05-28 17:38:01 +0700

    Update clang-setup.sh

--- clang-setup.sh
+++ clang-setup.sh
@@ -11,23 +11,21 @@
 # Export KERNELDIR as en environment-wide thingy
 # We start in scripts, so like, don't clone things
 KERNELDIR="$(pwd)"
+TC_DIR="${LOCAL_DIR}toolchain"
+CLANG_DIR="${TC_DIR}/clang-rastamod"
 SCRIPTS=${KERNELDIR}/kernelscripts
 OUTDIR=${KERNELDIR}/out
 COMPILER_TYPES=clang
+GCC_DIR="${LOCAL_DIR}toolchain/aarch64-linux-android-4.9" # Doesn't needed if use proton-clang
+GCC32_DIR="${LOCAL_DIR}toolchain/arm-linux-androideabi-4.9" # Doesn't needed if use proton-clang
 
 # Pick your poison
 if [[ "${COMPILER_TYPES}" =~ "clang" ]]; then
-        git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b android15-release --depth=1  "${KERNELDIR}/clang"
-	git clone https://github.com/Kyvangka1610/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu --depth=1 "${KERNELDIR}/gcc"
-        git clone https://github.com/Kyvangka1610/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf --depth=1 "${KERNELDIR}/gcc32"
-	COMPILER_STRING='google clang 17'
-        COMPILER_TYPE='google clang 17'
-else
-        # Default to GCC from Arter
-        git clone https://github.com/mvaisakh/gcc-arm64 -b gcc-master --depth=1 "${KERNELDIR}/gcc"
-        git clone https://github.com/kdrag0n/arm-eabi-gcc --depth=1 "${KERNELDIR}/gcc32"
-        COMPILER_STRING='GCC'
-	COMPILER_TYPE='GCC'
+        git clone --depth=1 -b clang-21.0 https://gitlab.com/kutemeikito/rastamod69-clang  "${CLANG_DIR}"
+	git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "${GCC_DIR}"
+    git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git "${GCC32_DIR}"
+	COMPILER_STRING='Rasta clang 21'
+        COMPILER_TYPE='Rasta clang 21'
 fi    
 
 export COMPILER_STRING COMPILER_TYPE KERNELDIR SCRIPTS OUTDIR
